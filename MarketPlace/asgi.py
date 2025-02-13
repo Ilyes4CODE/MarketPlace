090b5200d80e048fba6c953bcb205db84e7ad16e
ilyes4CODE <bendaikha.ilyes@univ-ouargla.dz>
2025-02-13 21:55:53 +0100

    commit

--- MarketPlace/asgi.py
+++ MarketPlace/asgi.py
@@ -6,9 +6,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from .middleware import JWTAuthMiddleware
+import django
   # Import your WebSocket consumer
 from channels.security.websocket import AllowedHostsOriginValidator
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MarketPlace.settings')
+django.setup() 
 django_asgi_app = get_asgi_application()
 
 from Chats import routing
