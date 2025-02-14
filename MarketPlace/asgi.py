66cfc532d4b918057b11d09f2bfb51c19ef02ec5
ilyes4CODE <bendaikha.ilyes@univ-ouargla.dz>
2025-02-14 16:40:53 +0100

    ya rabi tsl7

--- MarketPlace/asgi.py
+++ MarketPlace/asgi.py
@@ -21,10 +21,12 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_routes
-            )
+        JWTAuthMiddleware(
+            AuthMiddlewareStack(
+                URLRouter(
+                    routing.websocket_routes
+                    )
+                )
         )
     ),
 })
